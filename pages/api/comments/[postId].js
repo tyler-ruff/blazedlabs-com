f074f8ea09043c9b520e75db31be62d74fa4e87a
Tyler Ruff <hello@blazed.space>
2023-08-06 16:16:29 -0400

    Update [postId].js

--- pages/api/comments/[postId].js
+++ pages/api/comments/[postId].js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
             await prisma.comment.create(data);
             return res.redirect(303, `${process.env.SITE_URL}blog/${postId}`);
         } catch(exception){
-            return res.status(500).json({ msg: "Unknown database error" })
+            return res.status(500).json({ msg: `Unknown database error: ${exception}` })
         }
         
         return res.status(500);
