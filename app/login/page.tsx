1e97bb83c7c3a9f0295d80eb63f64ec4f4983e8d
tyler-ruff <hello@blazed.space>
2024-12-23 05:10:42 -0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,10 @@
 import { Metadata } from "next";
-import { redirect } from 'next/navigation';
 
 import Login from "@/components/login";
-import { url } from "@/config/app";
 
-export async function generateMetadata(): Promise<Metadata>{
-  const loggedIn = (await fetch(new URL('/api/login', url), {
-    method: "GET"
-  }));
-  const data = (await loggedIn.json());
-  if(data.isLogged === "true"){
-    redirect('/');
-  }
-  return {
-    title: "Login",
-    description: "Login to Blazed Labs."
-  }
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login to Blazed Labs.'
 }
 
 export default function LoginPage() {
