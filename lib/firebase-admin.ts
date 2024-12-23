194e066da95bb705093cc024cd74addc0ad2df64
tyler-ruff <hello@blazed.space>
2024-12-23 05:07:12 -0500

    quick fix

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,7 @@ const firebaseAdminConfig = {
     credential: cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
     })
 }
 
