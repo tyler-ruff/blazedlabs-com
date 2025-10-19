b993d1cd6917c314122f0fda465ad410e3e2a3d6
tyler-ruff <hello@blazed.space>
2025-10-19 07:10:02 -0400

    Update algolia.ts

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -3,6 +3,6 @@ import algoliasearch from 'algoliasearch/lite';
 const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID || "", process.env.ALGOLIA_API_KEY || "");
 
 //Index for blog
-const blogSearchIndex = algoliaClient.initIndex('posts');
+const blogSearchIndex = algoliaClient.initIndex('blazedlabs_com_blog');
 
 export default blogSearchIndex;
